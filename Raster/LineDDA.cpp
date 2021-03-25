9ca692e994da124e0efff7c462c12b34107f943e
chauminhnguyen <18520519@gm.uit.edu.vn>
2021-03-25 21:43:02 +0700

    updated

--- Raster/LineDDA.cpp
+++ Raster/LineDDA.cpp
@@ -31,19 +31,19 @@ void LineDDA::LineDDA2(CDC* pDC, int x1, int y1, int x2, int y2, COLORREF color)
 
 void LineDDA::DDA(CDC* pDC, int x1, int y1, int x2, int y2, COLORREF color) {
 	pDC->SetPixel(x1, y1, color);
-	int x = x1;
-	int y = y1;
-	int dx = x2 - x1;
-	int dy = y2 - y1;
-	int steps = abs(dx) > abs(dy) ? abs(dy) : abs(dx);
+	float x = x1;
+	float y = y1;
+	float dx = x2 - x1;
+	float dy = y2 - y1;
+	float steps = abs(dx) < abs(dy) ? abs(dy) : abs(dx);
 	float m = 1.0 * dy / steps;
 	float k = 1.0 * dx / steps;
 	for (int i = 0; i <= steps; i++)
 	{
 		y += m;
 		x += k;
-		y = round(y);
-		x = round(x);
-		pDC->SetPixel(x, y, color);
+		//y = round(y);
+		//x = round(x);
+		pDC->SetPixel(int(x + 0.5), int(y + 0.5), color);
 	}
 }
